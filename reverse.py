c85250f25f2a0d049daf214d4108823a592783c5
naveen9344793948 <92908836+naveen9344793948@users.noreply.github.com>
2021-10-22 10:40:24 +0600

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,6 @@
+from array import *
+array_num = array('i',[ 1,3,5,7,9,3])
+print("original array: "+str(array_num))
+array_num.reverse()
+print("Reverse the order of the items:")
+print(str(array_num))
